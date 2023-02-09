12a36f9aab0b88be58f8a802d60c2c384cab6f49
SekarMano14 <sekarmano46@gmail.com>
2023-02-09 19:32:16 +0530

    Mano's code

--- src/test/java/org/test/SampleMain.java
+++ src/test/java/org/test/SampleMain.java
@@ -3,6 +3,9 @@ package org.test;
 public class SampleMain {
 	public static void main(String[] args) {
 		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		
 
 	}
 
